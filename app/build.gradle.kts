a3581131b33f5eb279dcfdf3c42e0aeb6f5fa0d6
Malte <132433059+MaeschDEV@users.noreply.github.com>
2025-03-21 22:19:07 +0100

    Versionnummer geupdated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 27
         targetSdk = 35
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
